7be6aa6f396e9af86af15907e8add8d601ccc3e9
soonyy13 <soonyy13@gmail.com>
2024-07-28 19:04:24 +0900

    Update site.config.js-config-link-revoke

--- site.config.js
+++ site.config.js
@@ -28,7 +28,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://soonsoonyy-notion-blog-soonsoonyys-projects.vercel.app",
+  link: "https://morethan-log.vercel.app",
   since: 2024, // If leave this empty, current year will be used.
   lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
